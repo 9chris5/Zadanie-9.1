91bc37d2da48374dcc9c26d022629953b332621a
Krzysiu <kheciek@gmail.com>
2017-09-19 21:27:41 +0200

    Added spaces

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 function getTriangleArea (a, h) {
-    if (a>0 && h>0) {
-        return (a * h / 2);
+    if (a > 0 && h > 0) {
+        return a * h / 2;
     }
     else {
-        return ("Nieprawidłowe dane");
+        return "Nieprawidłowe dane";
     }
 }
 var triangle1Area = getTriangleArea(10,6);
 var triangle2Area = getTriangleArea(5,2);
 var triangle3Area = getTriangleArea(12,4);
-console.log("Pole 1: " + triangle1Area + ", pole 2: " + triangle2Area + ", pole 3: " + triangle3Area + ".");
+var triangle4Area = getTriangleArea(0,-1);
+console.log("Pole 1: " + triangle1Area + ", pole 2: " + triangle2Area + ", pole 3: " + triangle3Area + ", pole 4: " + triangle4Area + ".");
